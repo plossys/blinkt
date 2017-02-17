ab9ef1fe7ff9714f6cbef73f0976e280e3f7d81c
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-17 16:24:30 +0100

    Test tagged build with package version

--- test.sh
+++ test.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+set -e
+
+# test version
+if [ -n $TRAVIS_TAG ]; then
+  packageVersion=$(cat package.json | jq -j .version)
+  if [ ! $TRAVIS_TAG == $packageVersion ]; then
+    echo "Travis tag $TRAVIS_TAG and package.json version $packageVersion differ!"
+    exit 1
+  fi
+fi
+
+# test image
 docker run --entrypoint node blinkt --version
