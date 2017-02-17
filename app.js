76b06a56463e0d503ec2b308fdd54887cf4dfdfe
Michael Scherer <michael.scherer@sealsystems.de>
2017-02-17 15:47:10 +0100

    Clear update interval on exit.

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 'use strict';
 
-var Blinkt = require('node-blinkt'),
+const Blinkt = require('node-blinkt'),
 leds = new Blinkt();
 
-var NUM_LEDS = 8;
-var brightness = 0.1;
+const NUM_LEDS = 8;
+const brightness = 0.1;
+
+let updateInterval;
 
 leds.setup();
 leds.clearAll();
 
-var lightsOff = function () {
+const lightsOff = function () {
   leds.setAllPixels(0, 0, 0, 0.1);
   leds.sendUpdate();
   leds.sendUpdate();
 }
 
-var signals = {
+const signals = {
   'SIGINT': 2,
   'SIGTERM': 15
 };
 
-function shutdown(signal, value) {
+const shutdown(signal, value) {
   console.log('Stopped by ' + signal);
+  clearInterval(updateInterval);
   lightsOff();
   setTimeout(() => { process.exit(0); }, 1000);
 }
@@ -32,26 +35,25 @@ Object.keys(signals).forEach(function (signal) {
   });
 });
 
-// ---- animation-loop
-var offset = 0;
-setInterval(function () {
-  let red, green, blue;
-  for (var i = 0; i < NUM_LEDS; i++) {
-    // [red, green, blue] = wheel(((i * 256 / NUM_LEDS) + offset) % 256);
-    // leds.setPixel(i, red, green, blue, brightness)
-    leds.setPixel(i, 100, 50, 200, .5)
-  }
-
-	offset++;
-  leds.sendUpdate();
-}, 1000 / 30);
-
-console.log('Rainbow started. Press <ctrl>+C to exit.');
-
 // generate rainbow colors accross 0-255 positions.
-function wheel(pos) {
+const wheel = function (pos) {
   pos = 255 - pos;
   if (pos < 85) { return [255 - pos * 3, 0, pos * 3]; }
   else if (pos < 170) { pos -= 85; return [0, pos * 3, 255 - pos * 3]; }
   else { pos -= 170; return [pos * 3, 255 - pos * 3, 0]; }
 }
+
+// ---- animation-loop
+let offset = 0;
+updateInterval = setInterval(function () {
+  let red, green, blue;
+  for (let i = 0; i < NUM_LEDS; i++) {
+    [red, green, blue] = wheel(((i * 256 / NUM_LEDS) + offset) % 256);
+    leds.setPixel(i, red, green, blue, brightness)
+  }
+  leds.sendUpdate();
+  leds.sendUpdate();
+  offset++;
+}, 1000 / 30);
+
+console.log('Rainbow started. Press <ctrl>+C to exit.');
