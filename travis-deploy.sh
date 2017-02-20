8c517b7ae3399ec3b80a5a89347f5d9cd94b86e5
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-20 09:11:30 +0100

    Add linux/arm64 to manifest

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -4,6 +4,12 @@ set -e
 docker tag blinkt plossys/blinkt:linux-$ARCH-$TRAVIS_TAG
 docker push plossys/blinkt:linux-$ARCH-$TRAVIS_TAG
 
+if [ $ARCH == "arm" ]; then
+  # workaround until Travis can build arm64
+  docker tag blinkt plossys/blinkt:linux-arm64-$TRAVIS_TAG
+  docker push plossys/blinkt:linux-arm64-$TRAVIS_TAG
+fi
+
 if [ $ARCH == "amd64" ]; then
   set +e
   echo "Waiting for other images plossys/blinkt:linux-arm-$TRAVIS_TAG"
@@ -22,13 +28,13 @@ if [ $ARCH == "amd64" ]; then
 
   echo "Pushing manifest plossys/blinkt:$TRAVIS_TAG"
   ./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm \
+    --platforms linux/amd64,linux/arm,linux/arm64 \
     --template plossys/blinkt:OS-ARCH-$TRAVIS_TAG \
     --target plossys/blinkt:$TRAVIS_TAG
 
   echo "Pushing manifest plossys/blinkt:latest"
   ./manifest-tool push from-args \
-    --platforms linux/amd64,linux/arm \
+    --platforms linux/amd64,linux/arm,linux/arm64 \
     --template plossys/blinkt:OS-ARCH-$TRAVIS_TAG \
     --target plossys/blinkt:latest
 fi
